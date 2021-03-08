d7bae17fc453df6f59fe177b893ca6bc5d36b175
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2021-03-08 17:11:38 +0000

    ReveresInt Challenge

--- CodeChallenges.Tests/ReverseIntegerTests.cs
+++ CodeChallenges.Tests/ReverseIntegerTests.cs
@@ -3,11 +3,80 @@ using Xunit;
 
 namespace CodeChallenges.Tests
 {
-    public class ReverseInteger
+    public class ReverseIntegerTests
     {
         [Fact]
-        public void Test1()
+        public void OneNumberReturnsNumber()
         {
+           //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(1);
+            //Assert
+            Assert.Equal(1,reversed);
+
+        }
+        [Fact]
+        public void TwoNumbersAreReveresed()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(12);
+            //Assert
+            Assert.Equal(21,reversed);
+
+        }
+        [Fact]
+        public void ThreeNumbersAreReveresed()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(123);
+            //Assert
+            Assert.Equal(321,reversed);
+
+        }
+        [Fact]
+        public void ZeroAtTheEndDoesNotCount()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(120);
+            //Assert
+            Assert.Equal(21,reversed);
+        }
+        [Fact]
+        public void ThreeMinusNumbersReturnRevresedAndStillMinus()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(-123);
+            //Assert
+            Assert.Equal(-321,reversed);
+        }
+        [Fact]
+        public void NumberReturnedIsNotHigherThanThe32BitIntegerRange()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //act
+            var reversed = calculation.Reverse(1534236469);
+            //Assert
+            Assert.Equal(0,reversed);
+        }
+        [Fact]
+        public void NumberReturnedIsNotLowerThanThe32BitIntegerRange()
+        {
+            //Arrange
+            var calculation = new ReverseInteger();
+            //Act
+            var reversed = calculation.Reverse(-1147483648);
+            //Assert
+            Assert.Equal(0,reversed);
         }
     }
 }
--- CodeChallenges/ReverseInteger.cs
+++ CodeChallenges/ReverseInteger.cs
@@ -4,5 +4,23 @@ namespace CodeChallenges
 {
     public class ReverseInteger
     {
+        public int Reverse(int numbers)
+        {
+
+            var n = Math.Abs(numbers);
+            var newNumber = 0;
+            while (n > 0)
+            {
+                newNumber = (newNumber * 10) + (n % 10);
+                n /= 10;
+            }
+
+            if (numbers < 0)
+            {
+                newNumber = 0 - newNumber;
+            }
+
+            return newNumber;
+        }
     }
 }
