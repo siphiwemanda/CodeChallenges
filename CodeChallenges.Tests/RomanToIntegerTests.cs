23834180653bf99f5116f8a7c8af74ea0812c59f
Siphiwe <siphiwe.manda@bepsystems.co.uk>
2021-03-10 17:46:51 +0000

    Roman to Integer
    Roman to Integer Submission accepted

--- CodeChallenges.Tests/RomanToIntegerTests.cs
+++ CodeChallenges.Tests/RomanToIntegerTests.cs
@@ -1,7 +1,148 @@
-﻿namespace CodeChallenges.Tests
+﻿using Xunit;
+
+namespace CodeChallenges.Tests
 {
     public class RomanToIntegerTests
     {
-        
+        [Fact]
+        public void RomanNumeralIReturnsOne()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("I");
+            //Assert
+            Assert.Equal(1, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralVReturnsFive()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("V");
+            //Assert
+            Assert.Equal(5, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralXReturns10()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("X");
+            //Assert
+            Assert.Equal(10, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralLReturnsFifty()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("L");
+            //Assert
+            Assert.Equal(50, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralCReturnsOneHundrad()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("C");
+            //Assert
+            Assert.Equal(100, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralDReturnsFiveHundrad()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("D");
+            //Assert
+            Assert.Equal(500, result);
+
+        }
+
+        [Fact]
+        public void RomanNumeralDReturnsOneThousand()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("M");
+            //Assert
+            Assert.Equal(1000, result);
+
+        }
+
+        [Fact]
+        public void IIIIReturnsThree()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("III");
+            //Assert
+            Assert.Equal(3, result);
+
+        }
+
+        [Fact]
+        public void IVReturnsFour()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("IV");
+            //Assert
+            Assert.Equal(4, result);
+
+        }
+        [Fact]
+        public void IXReturnsNine()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("IX");
+            //Assert
+            Assert.Equal(9, result);
+
+        }
+        [Fact]
+        public void LVIIIReturnsFiftyEight()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("LVIII");
+            //Assert
+            Assert.Equal(58, result);
+
+        }
+        [Fact]
+        public void MCMXCIVReturnsFiftyOneHundradandNintyFour()
+        {
+            //Arrange
+            var romans = new RomanToInteger();
+            //Act
+            var result = romans.RomanToInt("MCMXCIV");
+            //Assert
+            Assert.Equal(1994, result);
+
+        }
     }
 }
--- CodeChallenges/RomanToInteger.cs
+++ CodeChallenges/RomanToInteger.cs
@@ -1,7 +1,60 @@
-﻿namespace CodeChallenges
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeChallenges
 {
     public class RomanToInteger
     {
-        
+
+        public int RomanToInt(string numbers)
+        {
+
+            char[] romanNumerals = numbers.ToCharArray();
+            List<int> numericalNumbers = new List<int>();
+            
+            var dictionary = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000},
+            };
+            var variationDictionary = new Dictionary<string, int>()
+            {
+                {"IV", 2},
+                {"IX", 2},
+                {"XL", 20},
+                {"XC", 20},
+                {"CD", 200},
+                {"CM", 200}
+            };
+
+            foreach (var numeral in romanNumerals)
+            {
+                foreach (var (key, value) in dictionary)
+                {
+                    if (numeral.Equals(key))
+                    {
+                        numericalNumbers.Add(value);
+                    }
+                }
+            }
+            var sum = numericalNumbers.Sum();
+            var x = 0; 
+            
+            foreach (var (key,value) in variationDictionary)
+            {
+               
+                if (numbers.Contains(key))
+                {
+                    x += value;
+                }
+            }
+            return sum - x;
+        }
     }
 }
